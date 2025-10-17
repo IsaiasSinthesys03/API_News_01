ae4e0b69d8442ff0e42e3473749354ada3268a9a
IsaiasSinthesys03 <braulioisaiasbernalpadron@gmail.com>
2025-10-17 03:25:39 -0600

    feat: ApiNews backend ready for prod (JWT, Swagger, CORS, dotenv) and docs updated

--- ApiNews/config.db.js
+++ ApiNews/config.db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize')
+require('dotenv').config();
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = require('./config.js');
 
-
-const connection = new Sequelize('db_news', 'root', '', {
-    host: 'localhost',
+const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
     dialect: 'mysql',
 })
-
 
 connection.authenticate()
     .then(() => {
